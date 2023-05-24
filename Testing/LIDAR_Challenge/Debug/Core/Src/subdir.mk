587e6f24f2a9dda698e08f130d117c02fa08547c
H-DIZZ <126120093+H-DIZZ@users.noreply.github.com>
2023-05-24 19:55:40 +1000

    Add files via upload

---
+++ Testing/LIDAR_Challenge/Debug/Core/Src/subdir.mk
@@ -0,0 +1,63 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (10.3-2021.10)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Core/Src/button_interrupt.c \
+../Core/Src/filter.c \
+../Core/Src/led.c \
+../Core/Src/main.c \
+../Core/Src/ptu_i2c.c \
+../Core/Src/serial.c \
+../Core/Src/serialise.c \
+../Core/Src/stm32f3xx_hal_msp.c \
+../Core/Src/stm32f3xx_it.c \
+../Core/Src/syscalls.c \
+../Core/Src/sysmem.c \
+../Core/Src/system_stm32f3xx.c \
+../Core/Src/timer.c 
+
+OBJS += \
+./Core/Src/button_interrupt.o \
+./Core/Src/filter.o \
+./Core/Src/led.o \
+./Core/Src/main.o \
+./Core/Src/ptu_i2c.o \
+./Core/Src/serial.o \
+./Core/Src/serialise.o \
+./Core/Src/stm32f3xx_hal_msp.o \
+./Core/Src/stm32f3xx_it.o \
+./Core/Src/syscalls.o \
+./Core/Src/sysmem.o \
+./Core/Src/system_stm32f3xx.o \
+./Core/Src/timer.o 
+
+C_DEPS += \
+./Core/Src/button_interrupt.d \
+./Core/Src/filter.d \
+./Core/Src/led.d \
+./Core/Src/main.d \
+./Core/Src/ptu_i2c.d \
+./Core/Src/serial.d \
+./Core/Src/serialise.d \
+./Core/Src/stm32f3xx_hal_msp.d \
+./Core/Src/stm32f3xx_it.d \
+./Core/Src/syscalls.d \
+./Core/Src/sysmem.d \
+./Core/Src/system_stm32f3xx.d \
+./Core/Src/timer.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Core/Src/%.o Core/Src/%.su: ../Core/Src/%.c Core/Src/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F303xC -c -I../Core/Inc -I../Drivers/STM32F3xx_HAL_Driver/Inc -I../Drivers/STM32F3xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32F3xx/Include -I../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+
+clean: clean-Core-2f-Src
+
+clean-Core-2f-Src:
+	-$(RM) ./Core/Src/button_interrupt.d ./Core/Src/button_interrupt.o ./Core/Src/button_interrupt.su ./Core/Src/filter.d ./Core/Src/filter.o ./Core/Src/filter.su ./Core/Src/led.d ./Core/Src/led.o ./Core/Src/led.su ./Core/Src/main.d ./Core/Src/main.o ./Core/Src/main.su ./Core/Src/ptu_i2c.d ./Core/Src/ptu_i2c.o ./Core/Src/ptu_i2c.su ./Core/Src/serial.d ./Core/Src/serial.o ./Core/Src/serial.su ./Core/Src/serialise.d ./Core/Src/serialise.o ./Core/Src/serialise.su ./Core/Src/stm32f3xx_hal_msp.d ./Core/Src/stm32f3xx_hal_msp.o ./Core/Src/stm32f3xx_hal_msp.su ./Core/Src/stm32f3xx_it.d ./Core/Src/stm32f3xx_it.o ./Core/Src/stm32f3xx_it.su ./Core/Src/syscalls.d ./Core/Src/syscalls.o ./Core/Src/syscalls.su ./Core/Src/sysmem.d ./Core/Src/sysmem.o ./Core/Src/sysmem.su ./Core/Src/system_stm32f3xx.d ./Core/Src/system_stm32f3xx.o ./Core/Src/system_stm32f3xx.su ./Core/Src/timer.d ./Core/Src/timer.o ./Core/Src/timer.su
+
+.PHONY: clean-Core-2f-Src
+
